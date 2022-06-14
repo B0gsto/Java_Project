b8980cc6ba0c0239196b5f4f33c8ae2c6bcaae4a
B0gsto <bogdan10andrei@gmail.com>
2022-06-14 14:02:22 +0300

    1

--- src/main/java/com/bogdan/demo/StoicaBogdanAndreiApplication.java
+++ src/main/java/com/bogdan/demo/StoicaBogdanAndreiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoicaBogdanAndreiApplication {
-
+		int a;
 	public static void main(String[] args) {
 		SpringApplication.run(StoicaBogdanAndreiApplication.class, args);
 	}
