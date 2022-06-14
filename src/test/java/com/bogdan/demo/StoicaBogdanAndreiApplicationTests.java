142d4f2f5892612c1ffe5ac86498663929d9ba21
B0gsto <bogdan10andrei@gmail.com>
2022-06-14 13:39:37 +0300

    1

---
+++ src/test/java/com/bogdan/demo/StoicaBogdanAndreiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoicaBogdanAndreiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
